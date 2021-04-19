1b8dcd8d23a7ab45d51696cdec93d17d09a786ad
CiMorgan <camorgan70@gmail.com>
2021-04-19 12:46:32 -0400

    commit 1

--- MathMasters.WebMVC/Controllers/AccountController.cs
+++ MathMasters.WebMVC/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using MathMasters.WebMVC.Models;
+using MathMasters.Data;
 
 namespace MathMasters.WebMVC.Controllers
 {
